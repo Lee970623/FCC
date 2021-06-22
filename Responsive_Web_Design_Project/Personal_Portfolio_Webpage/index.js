5b47019b199ba124cf737fd73d9adb7944e67531
Lee970623 <18970978999@163.com>
2021-06-22 18:49:02 +0800

    add animation when mouse over image

--- Responsive_Web_Design_Project/Personal_Portfolio_Webpage/index.js
+++ Responsive_Web_Design_Project/Personal_Portfolio_Webpage/index.js
@@ -4,10 +4,14 @@ window.onload = function() {
     image_links.forEach(link => {
         link.addEventListener('mouseover', () => {
             link.querySelector('.project-image').style.transform = 'scale(1.2)'
+            link.querySelectorAll('code')[0].style.opacity = '1'
+            link.querySelectorAll('code')[1].style.opacity = '1'
         })
 
         link.addEventListener('mouseleave', () => {
             link.querySelector('.project-image').style.transform = ''
+            link.querySelectorAll('code')[0].style.opacity = '0'
+            link.querySelectorAll('code')[1].style.opacity = '0'
         })
     })
 }
