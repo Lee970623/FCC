fe3a194ab6071d9dddc00bc3bb07d8ca3ce73cca
Lee970623 <18970978999@163.com>
2021-06-25 21:31:38 +0800

    completed telephone check

--- JS_Algorithms_And_Data_Structure/telephoneCheck.js
+++ JS_Algorithms_And_Data_Structure/telephoneCheck.js
@@ -1,17 +1,19 @@
 /* check if str is the correct American phone number
-    A correct phone number must include area code, wrapped in ()
-    coutry code is not necessary, but it must be 1 if it exists
-    str must include number
-    str can include space or hyphen*/
+ *  A correct phone number must include area code, wrapped in ()
+ *  coutry code is not necessary, but it must be 1 if it exists
+ *  str must include number
+ *   str can include space or hyphen
+ */
 function telephoneCheck(str) {
   /*
-    @params:
-        str: input string
-    return:
-        true or false
-    */
-    // TODO
-  let regex = /\d/g;
-
-  return regex.test(str);
+   * @params:
+   *    str: input string
+   * @return:
+   *    true or false
+   */
+  return /^1?\s?(\(\d{3}\)|\d{3})[\s\-]*\d{3}[\s\-]*\d{4}$/.test(str);
 }
+
+console.log(telephoneCheck("1(555)555-5555"));
+console.log(telephoneCheck("(555)555-5555"));
+console.log(telephoneCheck("1 555)555-5555"));
