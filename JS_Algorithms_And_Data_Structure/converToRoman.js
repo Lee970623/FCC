bd3430b2ea5ae7af9ae5d69f07c179cfdce78de7
Lee970623 <18970978999@163.com>
2021-06-25 20:04:09 +0800

    completed 'convert to roman'

--- JS_Algorithms_And_Data_Structure/converToRoman.js
+++ JS_Algorithms_And_Data_Structure/converToRoman.js
@@ -6,19 +6,48 @@ function convertToRoman(num) {
     @return:
         roman numeral
     */
-   // TODO
   const numToRoman = {
-    '1': "I",
-    '2': "II",
-    '3': "III",
-    '4': "IV",
-    '5': "V",
-    '6': "VI",
-    '7': 'VII',
-    '200': 'CC',
-    '30': 'XXX',
-    '1000': 'M'
+    1: "I",
+    2: "II",
+    3: "III",
+    4: "IV",
+    5: "V",
+    6: "VI",
+    7: "VII",
+    8: "VIII",
+    9: "IX",
+    10: "X",
+    20: "XX",
+    30: "XXX",
+    40: "XL",
+    50: "L",
+    60: "LX",
+    70: "LXX",
+    80: "LXXX",
+    90: "XC",
+    100: "C",
+    200: "CC",
+    300: "CCC",
+    400: "CD",
+    500: "D",
+    600: "DC",
+    700: "DCC",
+    800: "DCCC",
+    900: "CM",
+    1000: "M",
   };
+  let numArr = String(num).split("");
+  // make a number e.g. 1024 to [1000, 0, 20, 4]
+  return numArr.map((item, index) => {
+    let temp = parseInt(item.padEnd(numArr.length - index, 0));
+    if (temp > 1000) {
+        return "M".repeat(temp / 1000)
+    } else if (temp !== 0) {
+        return numToRoman[temp]
+    } else 
+        return ''
+  }).join('')
 }
 
-console.log(convertToRoman(1004))
+console.log(convertToRoman(2024)); // MMXXIV
+console.log(convertToRoman(3999)); // MMMCMXCIX
