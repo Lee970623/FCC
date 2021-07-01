13192883985b37990f2d92c05201fdfd380759b2
Lee970623 <18970978999@163.com>
2021-07-01 15:16:25 +0800

    image can be shown in normal size

--- Front_End_Libraries_Projects/simple-markdown-app/src/markdown/Markdown.js
+++ Front_End_Libraries_Projects/simple-markdown-app/src/markdown/Markdown.js
@@ -11,7 +11,7 @@ export default class Markdown extends React.Component {
     super();
     this.state = {
       input:
-        "# header 1 \n\n ## header 2 \n\n ### header3 \n\n *this is italic* \n\n `function() { console.log('Hello World!') }` \n\n There's also [links](https://www.freecodecamp.org), and \n\n > Block Quotes! \n\n And if you want to get really crazy, even tables: \n\n Wild Header | Crazy Header | Another Header?\n------------ | ------------- | -------------\nYour content can | be here, and it | can be here.... \n And here. | Okay. | I think we get it. \n\n - And of course there are lists. \n\n\t  - Some are bulleted. \n\n\t\t - With different indentation levels. \n\n 1. numbered List \n 1. second one. \n 1. last one. \n\n",
+        "# header 1 \n\n ## header 2 \n\n ### header3 \n\n *this is italic* \n\n `function() { console.log('Hello World!') }` \n\n There's also [links](https://www.freecodecamp.org), and \n\n > Block Quotes! \n\n And if you want to get really crazy, even tables: \n\n Wild Header | Crazy Header | Another Header?\n------------ | ------------- | -------------\nYour content can | be here, and it | can be here.... \n And here. | Okay. | I think we get it. \n\n - And of course there are lists. \n\n\t  - Some are bulleted. \n\n\t\t - With different indentation levels. \n\n 1. numbered List \n 1. second one. \n 1. last one. \n\n ![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)",
     };
 
     this.handleInput = this.handleInput.bind(this);
